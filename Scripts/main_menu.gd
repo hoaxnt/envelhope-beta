493b53ef9c3689a4788b318a3e6707e997ded0b4
user.email <regacho.marcjulius@gmail.com>
2025-05-28 02:59:27 +0800

    initial commit

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,10 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time
 func _ready():
-	get_tree().change_scene_to_file("res://Scenes/Chapter2.tscn")
-	print("Chapter2 loaded")
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://scenes/chapter_2.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
