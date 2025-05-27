a15e0b50e6946bc306c7637a5eac5ccb7e6eafe1
user.email <regacho.marcjulius@gmail.com>
2025-05-27 16:20:05 +0800

    initial commit

--- Scripts/Chapters/Chapter3.gd
+++ Scripts/Chapters/Chapter3.gd
@@ -1,11 +1,12 @@
-extends Node2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
+	print("Chapter3")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	pass
