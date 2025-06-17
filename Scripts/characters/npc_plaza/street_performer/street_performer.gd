04638bbab76c73399d1c49fc52d175913ac1744e
user.email <regacho.marcjulius@gmail.com>
2025-06-17 23:59:26 +0800

    added NPC scripts

--- Scripts/characters/npc_plaza/street_performer/street_performer.gd
+++ Scripts/characters/npc_plaza/street_performer/street_performer.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	connect("body_entered", self.interact)
 	
 	pass # Replace with function body.
 
@@ -9,3 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _interact(player):
+	print("Hello " + player.name)
