54c2480722d1ee14758593959cd013f017573ca3
user.email <regacho.marcjulius@gmail.com>
2025-06-19 00:25:58 +0800

    fix: street performer script

--- Scripts/characters/npc_plaza/street_performer/street_performer.gd
+++ Scripts/characters/npc_plaza/street_performer/street_performer.gd
@@ -3,7 +3,8 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	connect("body_entered", self.interact)
-
+	#get_child(2).text = self.name
+	print(get_child(1).name)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
