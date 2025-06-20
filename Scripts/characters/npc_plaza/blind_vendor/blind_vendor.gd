4a50bd41a8dedc47923bbf044659e144c66a2fdf
user.email <regacho.marcjulius@gmail.com>
2025-06-20 11:38:44 +0800

    fix: blind vendor script

--- Scripts/characters/npc_plaza/blind_vendor/blind_vendor.gd
+++ Scripts/characters/npc_plaza/blind_vendor/blind_vendor.gd
@@ -6,6 +6,12 @@ var is_dialog_visible = false
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("interact"):
+		is_dialog_visible = !is_dialog_visible
+		dialog_box = get_parent().get_child(6).get_child(0).get_child(1)
+		dialog_box.get_child(0).text = "I'm the " + self.name + "."
+		dialog_box.visible = not is_dialog_visible
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
