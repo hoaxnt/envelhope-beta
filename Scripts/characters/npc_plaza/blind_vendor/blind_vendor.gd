f52e098c06f15133cae140e9add8af597328b515
user.email <regacho.marcjulius@gmail.com>
2025-06-20 11:34:51 +0800

    fix: guard script

--- Scripts/characters/npc_plaza/blind_vendor/blind_vendor.gd
+++ Scripts/characters/npc_plaza/blind_vendor/blind_vendor.gd
@@ -1,6 +1,7 @@
 extends Node
 
-
+var dialog_box
+var is_dialog_visible = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
