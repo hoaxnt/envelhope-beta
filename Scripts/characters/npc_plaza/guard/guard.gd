d67e09a56514069f7917d0e05b119ed436209fbb
user.email <regacho.marcjulius@gmail.com>
2025-06-20 09:50:55 +0800

    minor fixes

--- Scripts/characters/npc_plaza/guard/guard.gd
+++ Scripts/characters/npc_plaza/guard/guard.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	connect("body_entered", self._on_body_entered)
 	pass # Replace with function body.
 
 
--- Scripts/characters/npc_plaza/street_performer/street_performer.gd
+++ Scripts/characters/npc_plaza/street_performer/street_performer.gd
@@ -2,12 +2,10 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	connect("body_entered", self.interact)
-	#get_child(2).text = self.name
-	print(get_child(1).name)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	connect("body_entered", self._on_body_entered)
 
-func interact(player):
+
+
+func _on_body_entered(player: PhysicsBody2D) -> void:
 	print("Hello " + player.name)
+	pass # Replace with function body.
