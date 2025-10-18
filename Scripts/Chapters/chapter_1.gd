34ba0607028041a502aebb77c61aca177943162d
user.email <regacho.marcjulius@gmail.com>
2025-10-19 01:35:52 +0800

    fix: resource loading error in hud.tscn

--- Scripts/Chapters/chapter_1.gd
+++ Scripts/Chapters/chapter_1.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 @onready var global_state = get_node("/root/GlobalState")
-@onready var hud_state = get_node("/root/HudState")
+@onready var hud = get_node("/root/HudState")
 
 func _ready() -> void:
-	hud_state.toggle_hud()
+	hud.toggle_hud()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("ui_cancel"):
