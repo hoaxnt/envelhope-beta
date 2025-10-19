2b8ab68057f89ca6d063d19f24d80d4810c0783b
user.email <regacho.marcjulius@gmail.com>
2025-10-20 01:03:15 +0800

    fix: non-existing resource bug

--- Scripts/Chapters/chapter_1.gd
+++ Scripts/Chapters/chapter_1.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
 @onready var global_state = get_node("/root/GlobalState")
-@onready var hud = get_node("/root/HudState")
 
 func _ready() -> void:
-	hud.toggle_hud()
+	pass
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("ui_cancel"):
---
+++ Scripts/utils/hud_elements.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func _on_bag_button_pressed() -> void:
+	print("bug")
+
+func _on_view_quest_button_pressed() -> void:
+	print("view quest")
