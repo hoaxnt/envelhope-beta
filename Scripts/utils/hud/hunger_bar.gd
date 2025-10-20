f011b7e4810b9045e543585c8546d9cbf046121e
user.email <regacho.marcjulius@gmail.com>
2025-10-20 15:54:23 +0800

    feat: added hunger bar script

--- Scripts/utils/hud/hunger_bar.gd
+++ Scripts/utils/hud/hunger_bar.gd
@@ -1,11 +1,14 @@
-extends Node
+extends ProgressBar
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+		var timer = $Timer 
+		timer.connect("timeout", _on_timer_timeout)
+		max_value = 100.0
+		value = 100.0
+		
+func _on_timer_timeout():
+		if value > 0:
+				value -= 1.0
+		else:
+				$Timer.stop()
+				print("Oops! player died from starving.")
