7aa15209acf6e97720ed56e634922af907426434
kajozxc <karljbm01@gmail.com>
2025-11-25 03:25:31 +0800

    map updates

--- Scripts/Chapters/chapter_1.gd
+++ Scripts/Chapters/chapter_1.gd
@@ -3,9 +3,26 @@ extends Node2D
 @onready var global_state = get_node("/root/GlobalState")
 @onready var CONFIG = SaveLoad.load_game(SaveLoad.CONFIG_PATH)
 @onready var tutorial = Hud.get_node("Tutorial")
+@onready var musicAudioStreamBG = $"AudioStreamPlayer-BGM"
+
+var backgroundMusicOn = true
 
 func _ready() -> void:
 	Hud.show()
+	
+func _process(_delta):
+	update_music_stats()
+	
+func update_music_stats():
+	if backgroundMusicOn:
+		if !musicAudioStreamBG.playing:
+			musicAudioStreamBG.play()
+	else:
+		musicAudioStreamBG.stop()
+		
+		
+			
+			
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("ui_cancel"):
