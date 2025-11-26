d8a00044e50484e23a312c12ea6d8a8e69bf8e42
user.email <regacho.marcjulius@gmail.com>
2025-11-27 00:06:38 +0800

    fix: food machine logic

--- Scripts/city_elements/food_machine.gd
+++ Scripts/city_elements/food_machine.gd
@@ -1,10 +1,27 @@
 extends Area2D
 
+@onready var PLAYER_DATA = SaveLoad.load_game(SaveLoad.PLAYER_DATA_PATH)
+var is_player_near_the_machine: bool
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		is_player_near_the_machine = true
 		$Label.show()
-
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		is_player_near_the_machine = false
 		$Label.hide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and is_player_near_the_machine:
+		if PLAYER_DATA["envelopes"] < 15:
+			$Label.text = "Not enough Envelope!"
+			$Label.modulate = Color(1, 0, 0, 1)
+			return
+		$Label.text = "Buy Food for $15 [E]"
+		$Label.modulate = Color(1, 1, 1, 1)
+			
+		PLAYER_DATA["envelopes"] -= 15
+		SaveLoad.save_game(PLAYER_DATA, SaveLoad.PLAYER_DATA_PATH)
+		
