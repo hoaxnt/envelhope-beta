280e4b5d2345a113d72d5dba1810b2c381c5773a
user.email <regacho.marcjulius@gmail.com>
2025-11-27 01:14:45 +0800

    minor fixes

--- Scripts/city_elements/junk_collector_minigame.gd
+++ Scripts/city_elements/junk_collector_minigame.gd
@@ -1,14 +1,19 @@
 extends Area2D
 
+var is_player_near: bool
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		is_player_near = true
 		$Label.show()
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		is_player_near = true
 		$Label.hide()
-
+		
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
-		pass
+	if event.is_action_pressed("interact") and is_player_near:
+		Hud.hide()
+		Transition.transition_to_scene("res://scenes/minigames/city/junk_collector.tscn")
--- Scripts/minigames/city/tambol.gd
+++ Scripts/minigames/city/tambol.gd
@@ -1,5 +1,11 @@
 extends Node2D
 @onready var instructions_panel = $CanvasLayer/Instructions
+@onready var anim = $CanvasLayer/AnimationPlayer
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("action"):
+		print("action")
 
 func _on_start_button_pressed() -> void:
 	instructions_panel.hide()
+	anim.play("countdown")
