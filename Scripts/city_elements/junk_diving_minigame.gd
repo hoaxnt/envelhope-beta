b2c6b6180812ca6c3431ef2d292b459af33d437b
user.email <regacho.marcjulius@gmail.com>
2025-11-27 04:03:43 +0800

    feat: tambol

--- Scripts/city_elements/junk_diving_minigame.gd
+++ Scripts/city_elements/junk_diving_minigame.gd
@@ -17,3 +17,5 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and is_player_near:
 		Hud.hide()
 		Transition.transition_to_scene("res://scenes/minigames/island/pearl_diving.tscn")
+		
+#		TASK: Day 4 Last Chase, Ending Story,PEARL diving Logic Hunger Logic, Hospital Scene, Saveload
