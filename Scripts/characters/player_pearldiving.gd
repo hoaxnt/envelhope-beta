48d4cce2124decbc680938f8757542a4390aadc2
user.email <regacho.marcjulius@gmail.com>
2025-11-28 16:35:37 +0800

    bug fixes

--- Scripts/characters/player_pearldiving.gd
+++ Scripts/characters/player_pearldiving.gd
@@ -1,22 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 80.0
-const ACCELERATION = 20.0
+const SPEED = 200.0#fortest
+const ACCELERATION = 50.0
 
 func _physics_process(delta: float) -> void:
 	var input_x := Input.get_axis("ui_left", "ui_right")
 	var input_y := Input.get_axis("ui_up", "ui_down")
-
 	var input_vector := Vector2(input_x, input_y).normalized()
-
 	var target_velocity := input_vector * SPEED
 
 	velocity.x = lerp(velocity.x, target_velocity.x, ACCELERATION * delta)
 	velocity.y = lerp(velocity.y, target_velocity.y, ACCELERATION * delta)
-
 	move_and_slide()
 
 func on_hit_obstacle() -> void:
 	#if game_manager and game_manager.game_active:
 		#game_manager.deduct_time()
+		print("CROCS BITE")
 		pass
--- Scripts/minigames/city/junk_diving.gd
+++ Scripts/minigames/city/junk_diving.gd
@@ -1,6 +1,46 @@
 extends Node2D
 
+@onready var anim = $CanvasLayer/AnimationPlayer
+@onready var summary_panel = $CanvasLayer/SummaryPanel
+@onready var summary_label = $CanvasLayer/SummaryPanel/HBoxContainer/VBoxContainer/SummaryLabel
+@onready var countdown_label = $CanvasLayer/Label
+@onready var instructions_panel = $CanvasLayer/Instructions
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+@onready var earnings_label: Label = $CanvasLayer/EarningsLabel
+
+
+var earnings = 0
+var sfx = StreamAudio.get_node("Sfx")
+
+var speed: float = 150.0
+var direction: int = 1
+var is_moving: bool = false
+
+
+func _ready():
+	instructions_panel.show()
+	summary_panel.hide()
+
+
+func _on_start_button_pressed() -> void:
+	instructions_panel.hide()
+	countdown_label.show()
+	anim.play("countdown")
+	await anim.animation_finished
+	countdown_label.hide()
+	#
+	#earnings_label.hide()
+	#is_moving = false
+	#
+	#summary_label.text = "You've earned %s Envelopes!" % str(earnings)
+	#
+	#var current_envelopes = GlobalData.get_player_data_value("envelopes")
+	#var new_total_envelopes = current_envelopes + earnings
+	#GlobalData.update_player_data("envelopes", new_total_envelopes)
+	#summary_panel.show()
+
+func _on_done_button_pressed() -> void:
+	#if player:
+		#player.position = GlobalData.load_player_position()
+	Transition.transition_to_scene("res://scenes/chapters/chapter_2.tscn")
