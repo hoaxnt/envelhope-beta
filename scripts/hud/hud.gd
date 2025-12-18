78ee99e3e9f1e16d951dbb3faa0c664bb733ed54
user.email <regacho.marcjulius@gmail.com>
2025-12-18 10:30:06 +0800

    minor fixes

--- scripts/hud/hud.gd
+++ scripts/hud/hud.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var sfx = StreamAudio.get_node("Sfx")
 @onready var inventory_panel = Hud.get_node("InventoryPanel")
 @onready var objective_label_anim = Hud.get_node("ObjectivePanel/MarginContainer/ObjectiveLabel/ObjectiveTextAnimation")
-
+@onready var action_button = $ActionButton
 signal action_button_pressed_signal 
 
 var is_action_pressed = false 
@@ -13,20 +13,39 @@ func _ready() -> void:
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("use"):
-		sfx.stream = StreamAudio.chop
-		sfx.play()
+		if GlobalData.player_data.get("equipped_tool") == "Chips":
+			GlobalData.player_data.set("equipped_tool", "")
+			InventoryManager.remove_item("Chips")
+			action_button.hide()
+		elif GlobalData.player_data.get("equipped_tool") == "Banana":
+			GlobalData.player_data.set("equipped_tool", "")
+			InventoryManager.remove_item("Banana")
+			action_button.hide()
+		elif GlobalData.player_data.get("equipped_tool") == "Water":
+			GlobalData.player_data.set("equipped_tool", "")
+			InventoryManager.remove_item("Water")
+			action_button.hide()
+		else:
+			sfx.stream = StreamAudio.chop
+			sfx.play()
 		action_button_pressed_signal.emit()
 
 func _on_action_button_pressed() -> void:
 	if GlobalData.player_data.get("equipped_tool") == "Chips":
+		GlobalData.player_data.set("equipped_tool", "")
 		InventoryManager.remove_item("Chips")
+		action_button.hide()
 	elif GlobalData.player_data.get("equipped_tool") == "Banana":
+		GlobalData.player_data.set("equipped_tool", "")
 		InventoryManager.remove_item("Banana")
+		action_button.hide()
 	elif GlobalData.player_data.get("equipped_tool") == "Water":
+		GlobalData.player_data.set("equipped_tool", "")
 		InventoryManager.remove_item("Water")
-		
-	sfx.stream = StreamAudio.chop
-	sfx.play()
+		action_button.hide()
+	else:
+		sfx.stream = StreamAudio.chop
+		sfx.play()
 	action_button_pressed_signal.emit()
 	
 func _on_bag_button_pressed() -> void:
