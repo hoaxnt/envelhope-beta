ddff9e0446de3cc7085e69089c5cd4a934edf68d
user.email <regacho.marcjulius@gmail.com>
2026-01-13 07:48:03 +0800

    minor fixes

--- scripts/city_elements/shop.gd
+++ scripts/city_elements/shop.gd
@@ -14,6 +14,13 @@ func _on_body_exited(body: Node2D) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		get_child(3).visible = true
+	if event.is_action_pressed("w") or event.is_action_pressed("a") or event.is_action_pressed("s") or event.is_action_pressed("d"):
+		get_child(3).visible = false
 		
 func _on_item_1_button_pressed() -> void:
-	print("Item1")
+	var current_envelopes = GlobalData.get_player_data_value("envelopes")
+	if current_envelopes < 100:
+		print("Not enough envelopes")
+		return
+		
+	print("Item1 bought")
