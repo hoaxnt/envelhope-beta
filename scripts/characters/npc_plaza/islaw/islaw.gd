2ec7692207af66252f5279de524b35c023677afe
user.email <regacho.marcjulius@gmail.com>
2026-01-16 13:37:15 +0800

    minor fixes

---
+++ scripts/characters/npc_plaza/islaw/islaw.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var current_body: String
+@onready var dialogue_box = Hud.get_node("DialogueBox")
+
+func _ready() -> void:
+	get_child(2).hide()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_child(2).show()
+		current_body = "player"
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_child(2).hide()
+		current_body = ""
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and current_body == "player":
+		var name_label : Label = dialogue_box.get_child(1).get_child(0).get_child(0)
+		var message_label : Label = dialogue_box.get_child(1).get_child(0).get_child(1)
+		if dialogue_box:
+			name_label.text = name
+			message_label.text = "Bagtas!"
+			dialogue_box.show()
