bdda2be65a881c1e3dc91aa7a768ad161a90aed8
user.email <regacho.marcjulius@gmail.com>
2026-01-16 12:11:08 +0800

    minor fixes

--- scripts/city_elements/shop.gd
+++ scripts/city_elements/shop.gd
@@ -7,26 +7,26 @@ var is_shop_entered = false
 var item1_price = 0
 
 func _ready() -> void:
-	get_child(3).visible = false
+	get_child(2).visible = false
 	not_enough.visible = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		current_body_entered = body
-		get_child(2).visible = true
+		get_child(1).visible = true
 		is_shop_entered = true
 		
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		current_body_entered = null
-		get_child(2).visible = false
+		get_child(1).visible = false
 		is_shop_entered = false
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and is_shop_entered:
-		get_child(3).visible = true
+		get_child(2).visible = true
 	if event.is_action_pressed("w") or event.is_action_pressed("a") or event.is_action_pressed("s") or event.is_action_pressed("d"):
-		get_child(3).visible = false
+		get_child(2).visible = false
 		not_enough.visible = false
 		
 func _on_item_1_button_pressed() -> void:
